1ac6c18bda1a6688f046960dc383a7206a3e2094
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-11-15 22:30:44 +0300

    Installation faker et fixture

--- src/Controller/BackController.php
+++ src/Controller/BackController.php
@@ -8,6 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BackController extends AbstractController
 {
+    #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
+    public function login(): Response
+    {
+        return $this->render('login.html.twig', [
+            'controller_name' => 'BackController',
+        ]);
+    }
+
     #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
