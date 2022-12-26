12cb4697885feaf1cf8d6a86d1b0a40ba43bfc58
Mendrika1284 <mendrikaitokiuana@gmail.com>
2022-12-26 13:48:25 +0300

    Api pour la préparation de la devis

--- src/Controller/API/DevisClientApiController.php
+++ src/Controller/API/DevisClientApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\API;
 
+use App\Entity\DevisClient;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
@@ -85,5 +86,77 @@ class DevisClientApiController extends AbstractController
         return new JsonResponse(['detailDevis' => $detailDevis,
                                  'detailUtilisateur' => $detailUtilisateur   
         ]);
+    }
+
+    /**
+     * @Route("/visualiserDevisArtisanApreparer/{id}", methods={"GET"})
+     */
+    public function visualiserDevisArtisanApreparer(int $id, ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+        $conn = $entityManager->getConnection();
+
+        $sqlForDevisClient = '
+                SELECT type_travaux.id as idTypeTravaux,
+                       type_travaux.nom as nomTypeTravaux, 
+                       devis_client.id as idDevis,
+                       devis_client.created_at as dateCreation,
+                       devis_client.position_x as devisPositionX,
+                       devis_client.position_y as devisPositionY,
+                       devis_client.info_supplementaire as detailDevis,
+                       devis_client.etat as etatDevis,
+                       devis_client.email as emailClient,
+                       devis_client.montant as montant,
+                       devis_client.date_debut as dateDebut,
+                       devis_client.date_fin as dateFin
+                FROM devis_client
+                JOIN type_travaux ON type_travaux.id = devis_client.id_type_travaux_id
+                JOIN artisan ON devis_client.id_artisan_id = artisan.id 
+                WHERE devis_client.id = :idDevis 
+            ';
+        $stmtForDevisClient = $conn->prepare($sqlForDevisClient);
+        $resultSetForDevisClient = $stmtForDevisClient->executeQuery(['idDevis'=> $id]);
+        $detailDevis = $resultSetForDevisClient->fetchAllAssociative();
+
+        $sqlForDetailUtilisateur = '
+                SELECT utilisateur.id as idUtilisateur,
+                       utilisateur.nom as nomUtilisateur,
+                       utilisateur.prenom as prenomUtilisateur,
+                       utilisateur.contact as contactUtilisateur
+                FROM utilisateur
+                WHERE utilisateur.email = :email 
+            ';
+        $stmtForDetailUtilisateur = $conn->prepare($sqlForDetailUtilisateur);
+        $resultSetForDetailUtilisateur = $stmtForDetailUtilisateur->executeQuery(['email'=> $detailDevis[0]['emailClient']]);
+
+        $detailUtilisateur = $resultSetForDetailUtilisateur->fetchAllAssociative();
+
+        return new JsonResponse(['detailDevis' => $detailDevis,
+                                 'detailUtilisateur' => $detailUtilisateur   
+        ]);
+    }
+
+
+    /**
+     * @Route("/preparerDevis/{id}/{montant}", methods={"PATCH"})
+     */
+    public function preparerDevis(int $id, String $montant, Request $request, ManagerRegistry $doctrine)
+    {
+        $devisClient = $doctrine->getRepository(DevisClient::class)->find($id);
+
+        if (!$devisClient) {
+            throw new NotFoundHttpException('Devis client non trouvé');
+        }
+
+        // Update the article properties as needed
+        $devisClient->setMontant($montant);
+        $devisClient->setEtat(2);
+
+        // Save the changes to the database
+        $em = $doctrine->getManager();
+        $em->persist($devisClient);
+        $em->flush();
+
+        return new JsonResponse(null, 204);
     }
 }
